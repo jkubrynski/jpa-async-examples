a451631a05b7fc7fe73b4d54c9e2ef138d104495
Jakub Kubrynski <jkubrynski@gmail.com>
2015-11-04 10:09:40 +0100

    Introduce spin update

--- src/main/java/com/kubrynski/blog/jpa/service/FirstNameUpdater.java
+++ src/main/java/com/kubrynski/blog/jpa/service/FirstNameUpdater.java
@@ -2,34 +2,55 @@ package com.kubrynski.blog.jpa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.kubrynski.blog.jpa.model.FieldUpdatedEvent;
+import com.kubrynski.blog.jpa.model.Person;
+import com.kubrynski.blog.jpa.repo.PersonRepository;
 
 /**
  * @author Jakub Kubrynski
  */
 @Service
-@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class FirstNameUpdater {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final PersonRepository personRepository;
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
+    private final TransactionTemplate transactionTemplate;
+
     @Autowired
-    public FirstNameUpdater(JdbcTemplate jdbcTemplate, ApplicationEventPublisher applicationEventPublisher) {
-        this.jdbcTemplate = jdbcTemplate;
+    public FirstNameUpdater(
+            PersonRepository personRepository,
+            ApplicationEventPublisher applicationEventPublisher,
+            PlatformTransactionManager transactionManager) {
+        this.personRepository = personRepository;
         this.applicationEventPublisher = applicationEventPublisher;
+        transactionTemplate = new TransactionTemplate(transactionManager);
     }
 
     @Async
-    public void update(String uuid, String newName) {
-        jdbcTemplate.update("UPDATE person SET first_name = ? WHERE uuid = ?", newName, uuid);
-        applicationEventPublisher.publishEvent(new FieldUpdatedEvent());
+    public void update(final String uuid, final String newName) {
+        while (true) {
+            try {
+                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+                    @Override
+                    protected void doInTransactionWithoutResult(TransactionStatus status) {
+                        Person person = personRepository.findByUuid(uuid);
+                        person.setFirstName(newName);
+                        applicationEventPublisher.publishEvent(new FieldUpdatedEvent());
+                    }
+                });
+                return;
+            } catch (OptimisticLockingFailureException ignore) {
+            }
+        }
     }
 }
--- src/main/java/com/kubrynski/blog/jpa/service/LastNameUpdater.java
+++ src/main/java/com/kubrynski/blog/jpa/service/LastNameUpdater.java
@@ -2,11 +2,13 @@ package com.kubrynski.blog.jpa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.kubrynski.blog.jpa.model.FieldUpdatedEvent;
 import com.kubrynski.blog.jpa.model.Person;
@@ -16,24 +18,40 @@ import com.kubrynski.blog.jpa.repo.PersonRepository;
  * @author Jakub Kubrynski
  */
 @Service
-@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class LastNameUpdater {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final PersonRepository personRepository;
 
     private final ApplicationEventPublisher applicationEventPublisher;
 
+    private final TransactionTemplate transactionTemplate;
+
     @Autowired
-    public LastNameUpdater(JdbcTemplate jdbcTemplate,
-            ApplicationEventPublisher applicationEventPublisher) {
-        this.jdbcTemplate = jdbcTemplate;
+    public LastNameUpdater(
+            PersonRepository personRepository,
+            ApplicationEventPublisher applicationEventPublisher,
+            PlatformTransactionManager transactionManager) {
+        this.personRepository = personRepository;
         this.applicationEventPublisher = applicationEventPublisher;
+        transactionTemplate = new TransactionTemplate(transactionManager);
     }
 
     @Async
-    public void update(String uuid, String newName) {
-        jdbcTemplate.update("UPDATE person SET last_name = ? WHERE uuid = ?", newName, uuid);
-        applicationEventPublisher.publishEvent(new FieldUpdatedEvent());
+    public void update(final String uuid, final String newName) {
+        while (true) {
+            try {
+                transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+                    @Override
+                    protected void doInTransactionWithoutResult(TransactionStatus status) {
+                        Person person = personRepository.findByUuid(uuid);
+                        person.setLastName(newName);
+                        applicationEventPublisher.publishEvent(new FieldUpdatedEvent());
+                    }
+                });
+                return;
+            } catch (OptimisticLockingFailureException ignore) {
+            }
+        }
     }
 
 }
